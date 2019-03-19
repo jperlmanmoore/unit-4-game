46c81eadfddb20f19cb60bcd7b2fbbddb158e8ba
jperlmanmoore <jperlmanmoore@gmail.com>
2019-03-18 23:18:47 -0400

    getting set up

---
+++ assets/javascript/game.js
@@ -0,0 +1,95 @@
+
+$(document).ready(function() {
+                    });
+
+
+//
+var game = {
+    characters: {
+        "Leia": {
+            name:   "Leia",
+            image: "leia.jpg", //check how to
+            attackPower:   1,
+            healthPoints:  1,
+            counterAttackPower: 1,
+    },
+
+        "DarthM": {
+            name: "Darth Maul",
+            image: "maul.jpg",
+            attackPower:   1,
+            healthPoints:  1,
+            counterAttackPower: 1,
+        },
+
+        "trooper": {
+            name: "Storm Trooper",
+            image: "trooper.jpg",
+            attackPower:   1,
+            healthPoints:  1,
+            counterAttackPower: 1,
+        },
+
+        "kylo": {
+            name: "Kylo Ren",
+            image: "kylo.jpg",
+            attackPower:   1,
+            healthPoints:  1,
+            counterAttackPower: 1,
+        },
+    },
+}
+
+//function startgame() {}
+    // -
+
+//function gameOver() {}
+    // --show message if won or lost
+    // --show start button
+
+//function userChooseChar() {}
+//function userTurn() {}
+
+//function userChooseOpponent() {}
+//function opponentTurn() {}
+
+//function figth() {}
+//function attackPower() {}
+//function counterAttackPower() {}
+
+//function to generate HP = random
+//function generateHP() {}
+//function to generate AP = random;
+//function generateAP() {}
+
+
+
+
+
+//$(document).ready(function) {
+    //gameOver = false;
+    // .on("click", function()) {
+        //if (!gameover) {
+            //
+        //}
+    //}
+//}
+
+// to change the entire page
+//$("body".addClass("whatever class"); {
+//});
+
+//to toggle class
+// $( "span" ).click(function() {
+//     $( "li" ).each(function() {
+//       $( this ).toggleClass( "example" );
+//     });
+//   });
+
+
+//.keyup & .off("keyup")
+
+//position absolute - characters
+//position relative -- board
+
+
