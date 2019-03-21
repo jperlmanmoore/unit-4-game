76c3fc0009b0cb8af7fa76beb3f9a42622708c2b
jperlmanmoore <jperlmanmoore@gmail.com>
2019-03-21 15:57:47 -0400

    working on game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,82 +1,152 @@
+//1. start with all chars in first array & generate/show stats at bottom of card
+//2. pick the character and keep that one in the pickUserCharArray & move others to the pickOpponentCharArry
+//3. from the pick OpponentArray, choose the frist opponent & move the first opponent to currentOpponentArray
+//4. do battle between userChar and opponentChar
+//5. pick second opponentChar & move to currentOppoentArray
+//6. pick thirs opponentChar & move to currentOpponentArray
+//7. action between user and opponents
+//8. show win or lose
+//9. restart game
 
-$(document).ready(function() {
-                    });
+$(document).ready(function () {
+    $("#leia").on("click", function () {
+        console.log("Leia was clicked!");
+    });
+    
+    $("#vader").on("click", function () {
+        console.log("Maul was clicked!");
+    });
+    
+    $("#trooper").on("click", function () {
+        console.log("trooper was clicked!");
+    });
+    
+    $("#kylo").on("click", function () {
+        console.log("kylo was clicked!");
+    });
+});
 
 var attackPower = 0;
 var healthPoints = 0;
 var counterAttackPower = 0;
+var charachterArray = []; //at start list all characters & leave user choice here
+var opponentChoiceArray = []; //move unpicked characters here
+var currentOpponentArray = []; //move picked opponent here
 
 
-//
-var game = {                    //characters are objects
-    characters: {
-        "Leia": {
-            name:   "Leia",
-            image: "leia.jpg",  //check how to write
-            attackPower:   1,
-            healthPoints:  1,
-            counterAttackPower: 1,
+var charachters = [
+    "#leia", {
+        //name:   "Leia"
+        attackPower: 5,
+        healthPoints: 200,
+        counterAttackPower: 30,
     },
 
-        "DarthM": {
-            name: "Darth Maul",
-            image: "maul.jpg",
-            attackPower:   1,
-            healthPoints:  1,
-            counterAttackPower: 1,
-        },
-
-        "trooper": {
-            name: "Storm Trooper",
-            image: "trooper.jpg",
-            attackPower:   1,
-            healthPoints:  1,
-            counterAttackPower: 1,
-        },
-
-        "kylo": {
-            name: "Kylo Ren",
-            image: "kylo.jpg",
-            attackPower:   1,
-            healthPoints:  1,
-            counterAttackPower: 1,
-        },
+    "#vader", {
+        //name: "Darth Vader"
+        attackPower: 10,
+        healthPoints: 200,
+        counterAttackPower: 10,
     },
-}
 
-//function startgame() {}
-    // -
+    "#trooper", {
+        //name: "Storm Trooper"
+        attackPower: 10,
+        healthPoints: 200,
+        counterAttackPower: 30,
+    },
 
-//function gameOver() {}
-    // --show message if won or lost
-    // --show start button
+    "#kylo", {
+        //name: "Kylo Ren"
+        attackPower: 5,
+        healthPoints: 200,
+        counterAttackPower: 20,
+    },
+]
 
-//function userChooseChar() {}
-//function userTurn() {}
 
-//function userChooseOpponent() {}
-//function opponentTurn() {}
+var opponentChoiceArray = 
 
-//function figth() {}
-//function attackPower() {}
-//function counterAttackPower() {}
+$("pickUserCharArray").splice("1, 2, 3");
+$("pickOpponentChar").add("pickUserCharArray");
 
-//function to generate HP = random
-//function generateHP() {}
-//function to generate AP = random;
-//function generateAP() {}
 
+// var selectedElements = [1,2,3,4,5].splice(0,2);  in javascript
+// var targetArray = ['a', 'b', 'c'].concat(selectedElements);
+
+
+
+
+
+
+$("#pickUserCharArray").prepend("<div id='pickInst'>Pick character</div>");
+$("#pickInst").css("font-family", 'Open Sans Condensed', 'sans-serif');
+
+$("#pickOpponentChar").prepend("<div id='pickInst2'>Pick opponent</div>");
+$("#pickInst2").css("font-family", 'Open Sans Condensed', 'sans-serif');
+
+$("#opponentChar").prepend("<div id='opponent'>Opponent</div>");
+$("#opponent").css("font-family", 'Open Sans Condensed', 'sans-serif');
+
+
+// $("pickUserChar").click(function() {
+//         $( this ).toggleClass( "off" );
+//       });
+//       var p;
+//       $( "button" ).click(function() {
+//         if ( p ) {
+//           p.appendTo( "body" );
+//           p = null;
+//         } else {
+//           p = $( "p" ).detach();
+//         }
+//       });
+
+//var new_array = old_array.concat([value1[, value2[, ...[, valueN]]]])  
+
+
+
+
+
+
+
+
+//$().appendTo( document.body ); //move the three to choose Opponent row
+
+// $('.searchbox-input').change( function () { //change to fire on click instead
+//     $('.card').show();
+//     var filter = $(this).val(); // get the value of the input, which we filter on
+//     $('.container').find(".card-title:not(:contains(" + filter + "))").parent().css('display','none');
+// });
+
+// Show all hidden divs using a slide down animation over 0.6 seconds
+//$( "div.hidden" ).slideDown( 600 );
+
+// Hide all paragraphs using a slide up animation over 0.8 seconds
+//$( "p" ).slideUp( 800 );
+
+
+
+//function userChooseOpponent() {}  //on click move the choice to the opponent array
+
+
+
+//function attackPower() {} //each character increases by its base on each attack
+
+//function counterAttack Power() {} //assign to enemy only & doesn't change
+
+//function healthPoints() {} //starts at amount and decreases
 
 
 
 
 //$(document).ready(function) {
-    //gameOver = false;
-    // .on("click", function()) {
-        //if (!gameover) {
-            //
-        //}
-    //}
+//gameOver = false;
+// .on("click", function()) {
+//if (!gameover) {
+//
+//}
+//}
 //}
 
 // to change the entire page
@@ -95,5 +165,3 @@ var game = {                    //characters are objects
 
 //position absolute - characters
 //position relative -- board
-
-
